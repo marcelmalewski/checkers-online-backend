186385da2223eec46fabd63c32b56e906fd0d671
marcel <mmalewski9@gmail.com>
2022-07-30 23:11:30 +0200

    yes

---
+++ src/main/java/com/marcel/malewski/checkersonlinebackend/CheckersOnlineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marcel.malewski.checkersonlinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckersOnlineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckersOnlineBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcel/malewski/checkersonlinebackend/CheckersOnlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcel.malewski.checkersonlinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckersOnlineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
