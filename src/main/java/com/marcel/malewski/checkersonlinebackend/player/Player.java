b147a0f3e327e2de99e248f7672fd81d4b1306fb
Marcel Malewski <mmalewski9@gmail.com>
2022-08-14 19:44:30 +0200

    yes

--- src/main/java/com/marcel/malewski/checkersonlinebackend/player/Player.java
+++ src/main/java/com/marcel/malewski/checkersonlinebackend/player/Player.java
@@ -1,7 +1,20 @@
 package com.marcel.malewski.checkersonlinebackend.player;
 
-public record Player(Integer id,
-                     String nickname,
-                     String password,
-                     Integer playersRoomId) {
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Player {
+   private Integer id;
+   private String nickname;
+   private String password;
+   private Integer playersRoomId;
+
+   public Player(int id, String nickname, String password, Integer playersRoomId) {
+      this.id = id;
+      this.nickname = nickname;
+      this.password = password;
+      this.playersRoomId = playersRoomId;
+   }
 }
